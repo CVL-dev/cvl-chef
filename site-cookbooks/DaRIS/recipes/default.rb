77bd746f3ba2d01b9db64505abc5ebb3034c2f69
Stephen Crawley <s.crawley@uq.edu.au>
2013-10-01 16:44:02 +1000

    Once again ...

--- site-cookbooks/DaRIS/recipes/default.rb
+++ site-cookbooks/DaRIS/recipes/default.rb
@@ -1,7 +1,1 @@
-node.normal['java']['install_flavor'] = 'openjdk'
-node.normal['java']['jdk_version'] = '7'
-node.normal['java']['accept_license_agreement'] = true
-
-# This workaround comes from CHEF-4234.  It forces the "java" recipe attributes
-# to be reloaded, and the ones that interpolate the above to be reevaluated.
-node.from_file(run_context.resolve_attribute(*parse_attribute_file_spec("java")))
+include_recipe "java"

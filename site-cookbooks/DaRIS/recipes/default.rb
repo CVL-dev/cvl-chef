f4dab60c7ede07f20a998a57208dec201dbaa994
Stephen Crawley <s.crawley@uq.edu.au>
2013-10-01 16:37:56 +1000

    Overriding the java attributes in a wrapper recipe is "tricky"

--- site-cookbooks/DaRIS/recipes/default.rb
+++ site-cookbooks/DaRIS/recipes/default.rb
@@ -1,1 +1,7 @@
-include_recipe "java::default"
+node.normal['java']['install_flavor'] = 'openjdk'
+node.normal['java']['jdk_version'] = '7'
+node.normal['java']['accept_license_agreement'] = true
+
+# This workaround comes from CHEF-4234.  It forces the "java" recipe attributes
+# to be reloaded, and the ones that interpolate the above to be reevaluated.
+node.from_file(run_context.resolve_attribute(*parse_attribute_file_spec("java")))
